efc15be6d4b7db72d324d4cf3a9a595c32d40947
DanCaveney267 <dancaveney9@gmail.com>
2025-10-07 18:45:53 +0100

    Add files via upload

---
+++ week1_lab.py
@@ -0,0 +1,14 @@
+print ("Hello, World!")
+
+Student_Name = "Daniel"
+
+Student_Age = 18
+
+print("Student Name:", Student_Name)
+print("student Age:", Student_Age)
+
+Num1 = 15
+Num2 = 10
+Total = Num1 + Num2
+
+print ("The sum is:", Total)
